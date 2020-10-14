2d5cfcee0e3a9e08cadfcb1757a4f3206ed3057b
Jack Wu <masterjack1997@gmail.com>
2020-10-14 14:04:54 -0400

    get questions

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,6 +8,17 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, questions): 
+  page = request.args.get('page', 1, type=int)
+  start = (page-1) * QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  selection = [question.format() for question in questions]
+  current_questions = selection[start:end]
+
+  return current_questions
+
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
@@ -39,6 +50,10 @@ def create_app(test_config=None):
   def get_categories():
     categories = Category.query.all()
     category_types = [category.type for category in categories] 
+
+    if len(category_types) == 0:
+      abort(404)
+
     return jsonify({
       'success': True, 
       'categories': category_types
@@ -57,6 +72,22 @@ def create_app(test_config=None):
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+
+  @app.route('/questions')
+  def get_questions():
+    questions = Question.query.all() 
+    total_questions = len(questions)
+    current_questions = paginate_questions(request, questions)
+
+    categories = Category.query.all()
+    category_types = [category.type for category in categories]
+
+    return jsonify({
+      'questions': current_questions,
+      'total_questions': total_questions,
+      'categories': category_types,
+      'current_category': 'test'
+    })
 
   '''
   @TODO: 
